1b631081c0904625e9bfdf28764a1bd3e8861919
Brian Vo <Vobrian025@gmail.com>
2023-05-10 18:16:32 -0400

    done

--- day1.sql
+++ day1.sql
@@ -29,22 +29,29 @@ GROUP BY staff_id
 ORDER BY COUNT(rental_id) DESC;
 
 
---6.)How many different distinct names are there? 591
-SELECT COUNT(distinct first_name)
-FROM customer;
+--6.)How many different district names are there? 378
+SELECT COUNT(DISTINCT district)
+FROM address;
 
 
 
---7.)What film has the most actors in it? (use film_actor table and get film_id) 5462?
-SELECT COUNT(film_id)
-FROM film_actor;
+--7.)What film has the most actors in it? (use film_actor table and get film_id) film 508 HAS 15 actors
+--LAMBS C
+SELECT film_id, COUNT(actor_id)
+FROM film_actor
+GROUP BY film_id
+ORDER BY COUNT(actor_id) DESC;
+
+SELECT film_id, title
+FROM film
+WHERE film_id = 508;
 
 
---8.)From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
---NOT CORRECT
-SELECT last_name
-FROM store_id 1;
-WHERE last_name ending with 'es';
+--8.)From store_id 1, how many customers have a last name ending with ‘es’? (use customer table) 13 last names in store 1
+SELECT COUNT(last_name), store_id
+FROM customer
+WHERE last_name LIKE  '_%es' AND store_id = 1
+GROUP BY store_id;
 
 --9.)How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 --with ids between 380 and 430? (use group by and having > 250)
